8147f4c36d32286dcc7f522b61e17699cf35dd39
Vincent Asante <vincenthokie@gmail.com>
2018-04-02 14:33:15 +0300

    ft(neededGems): Ensure only needed gems are installed on production and staging - refactor setup script for packer image, ensure only needed gems are installed in production   and staging images [Finishes #156368198]

--- packer/setup.sh
+++ packer/setup.sh
@@ -11,7 +11,14 @@ create_vof_user() {
 
 setup_vof_code() {
   sudo chown -R vof:vof /home/vof 
-  cd /home/vof/app && bundle install
+  cd /home/vof/app
+
+  # ensure only the required gems are installed on production and staging
+  if [[ "$RAILS_ENV" == "production" || "$RAILS_ENV" == "staging" ]]; then
+    bundle install --without development test sandbox
+  else
+    bundle install  
+  fi
 }
 
 start_supervisor_service() {
