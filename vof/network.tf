c996c3d3124946e70f7bf1c0c4c53bf104bfdf76
Vincent Asante <vincenthokie@gmail.com>
2018-06-04 17:49:07 +0300

    test prod deployment

--- vof/network.tf
+++ vof/network.tf
@@ -8,3 +8,15 @@ resource "google_compute_subnetwork" "vof-private-subnetwork" {
   network = "${google_compute_network.vof-network.self_link}"
   ip_cidr_range = "${var.ip_cidr_range}"
 }
+
+resource "google_compute_network_peering" "elk-peering" {
+  name = "${var.env_name}-to-elk-peering"
+  network = "${google_compute_network.vof-network.self_link}"
+  peer_network = "projects/${var.project_id}/global/networks/vof-elk-network"
+}
+
+resource "google_compute_network_peering" "elk-peering-complete" {
+  name = "elk-to-${var.env_name}-peering"
+  network = "projects/${var.project_id}/global/networks/vof-elk-network"
+  peer_network = "${google_compute_network.vof-network.self_link}"
+}
